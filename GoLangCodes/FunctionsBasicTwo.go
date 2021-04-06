82ea6e5cf7e890887d872abfd67a24fd005c77e1
Aditya Mishra <mishraaaditya29@gmail.com>
2021-04-06 19:35:44 +0530

    Passing Multiple Parameters In A Function

---
+++ GoLangCodes/FunctionsBasicTwo.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i < 5; i++ {
+		sayMessage("Hello Go!", i)
+	}
+}
+
+func sayMessage(msg string, idx int) {
+	fmt.Println(msg)
+	fmt.Println("The value of the index is", idx)
+
+}
