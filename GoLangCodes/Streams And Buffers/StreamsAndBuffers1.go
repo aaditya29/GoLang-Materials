2e21080f767edcd294b97a460ecef48c4e6899bd
Aditya Mishra <mishraaaditya29@gmail.com>
2021-06-17 18:42:56 +0530

    Code Commit

--- GoLangCodes/Streams And Buffers/StreamsAndBuffers1.go
+++ GoLangCodes/Streams And Buffers/StreamsAndBuffers1.go
@@ -1,1 +1,6 @@
 
+/*
+A data stream as the name suggests is a stream of data.
+In layman terms we can say this as the stream of water flowing from one end to another.
+Each droplet of water is the packet of data flowing in the stream and we can resize this data packet as per our needs.
+*/
